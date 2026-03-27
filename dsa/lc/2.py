9f4f78bf1cd770194a9a797bde4c92cd9ca1bc1c
Otito Amuga <urphasin@gmail.com>
2026-03-26 22:23:52 -0600

    xzxzx

--- dsa/lc/2.py
+++ dsa/lc/2.py
@@ -5,3 +5,25 @@ class ListNode:
 
 
 
+def addTwoNumbers(l1: ListNode, l2: ListNode):
+    pass
+
+
+M_0 = ListNode(2)
+M_1 = ListNode(4)
+M_2 = ListNode(3)
+
+N_0 = ListNode(5)
+N_1 = ListNode(6)
+N_2 = ListNode(4)
+
+
+M_0.next = M_1
+M_1.next = M_2
+
+N_0.next = N_1
+N_1.next = N_2
+
+
+if __name__ == "__main__":
+    addTwoNumbers(M_0, N_0)
