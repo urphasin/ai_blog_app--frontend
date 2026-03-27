44ce78b80d1c8b60354f32a9a70fc425637b76ab
Otito Amuga <urphasin@gmail.com>
2026-03-26 23:11:33 -0600

    fgf

--- dsa/lc/2.cpp
+++ dsa/lc/2.cpp
@@ -9,6 +9,35 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-int main() {
+void traverse(ListNode* L) {
     
+}
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+
+    return new ListNode();
+}
+
+int main() {
+// M list: 2 -> 4 -> 3
+    ListNode* M_0 = new ListNode(2);
+    ListNode* M_1 = new ListNode(4);
+    ListNode* M_2 = new ListNode(3);
+
+    M_0->next = M_1;
+    M_1->next = M_2;
+
+    // N list: 5 -> 6 -> 4
+    ListNode* N_0 = new ListNode(5);
+    ListNode* N_1 = new ListNode(6);
+    ListNode* N_2 = new ListNode(4);
+
+    N_0->next = N_1;
+    N_1->next = N_2;
+
+    addTwoNumbers(M_0, N_0);
+
+
+    return 0;
 }
