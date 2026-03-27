fb3583174ed5ffe4b644536f000e354d89b053aa
Otito Amuga <urphasin@gmail.com>
2026-03-26 22:58:11 -0600

    dfdf

--- dsa/lc/2.py
+++ dsa/lc/2.py
@@ -2,6 +2,11 @@
 You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+M: 2 → 4 → 3   = 342
+N: 5 → 6 → 4   = 465
+
+342 + 465 = 807 → output: 7 → 0 → 8
 """
 
 class ListNode:
